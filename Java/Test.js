9a1d089942ff711d78aa1acc21918269365d68ed
Googles1 <107815561+Googles1@users.noreply.github.com>
2022-06-19 17:03:34 -0500

    Add files via upload

---
+++ Java/Test.js
@@ -0,0 +1,8 @@
+const openBtn = document.getElementById('open');
+openBtn.addEventListener('click',function()
+{// Code run upon 'open' click 
+window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley') 
+})
+git; remote: add: origin: https://github.com/Googles1/Very-poggies-name.git
+git; branch -M;main
+git; push -u; origin; main
